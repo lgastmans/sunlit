c4ea15d3efbb70ebdf78dca19422f33e067a0085
Luk <lgastmans@gmail.com>
2021-06-30 15:30:36 +0530

    migrations related to Products table

---
+++ database/migrations/2021_06_30_051405_create_suppliers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('state_id');
+            $table->string('company_title');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip');
+            $table->string('district');
+            $table->string('gstin');
+            $table->string('contact_person');
+            $table->string('phone');
+            $table->string('phone2');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
