a178d6080c617ac20aef06f9827266652ef3ceb2
Luk <lgastmans@gmail.com>
2021-06-30 11:37:56 +0530

    product table migration

--- database/migrations/2021_06_29_095128_create_products_table.php
+++ database/migrations/2021_06_29_095128_create_products_table.php
@@ -15,7 +15,22 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
+            $table->foreignId('supplier_id');
+            $table->foreignId('tax_id');
+            $table->string('code')->unique();
+            $table->string('description');
+            $table->string('model');
+            $table->string('cable_length');
+            $table->string('kw_rating');
+            $table->string('part_number');
+            $table->text('notes');
             $table->timestamps();
+            $table->softDeletes();
+
+            // $table->foreign('category_id')->references('id')->on('category');
+            // $table->foreign('supplier_id')->references('id')->on('supplier');
+            // $table->foreign('tax_id')->references('id')->on('tax');
         });
     }
 
