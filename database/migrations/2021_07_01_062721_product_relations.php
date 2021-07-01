e8497d909836e37cf03672110052f163080f66fa
Luk <lgastmans@gmail.com>
2021-07-01 12:02:05 +0530

    products related foreign keys

---
+++ database/migrations/2021_07_01_062721_product_relations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductRelations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+            $table->foreign('tax_id')->references('id')->on('taxes');
+        });
+
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
