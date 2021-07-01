45bed87761002d4f552518f8e8530d9a4aaadda3
Luk <lgastmans@gmail.com>
2021-07-01 15:52:31 +0530

    added SoftDeletes to Models

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Supplier extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 }
