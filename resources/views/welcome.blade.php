b073844ed7f7a0b086c6d816e685d144b19b0b60
Quentin <quentin@octostudio.fr>
2021-07-02 12:06:05 +0530

    move body tag to app file, remove breadcrumb and footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-    <body class="loading" data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
         <!-- Begin page -->
         <div class="wrapper">
             @include('layout/left-sidebar')
@@ -23,14 +22,8 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                                            <li class="breadcrumb-item active">Starter</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Starter</h4>
+
+                                    <h4 class="page-title">Home page</h4>
                                 </div>
                             </div>
                         </div>     
@@ -45,14 +38,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6">
-                                <script>document.write(new Date().getFullYear())</script> © Hyper - Coderthemes.com
-                            </div>
-                            <div class="col-md-6">
-                                <div class="text-md-end footer-links d-none d-md-block">
-                                    <a href="javascript: void(0);">About</a>
-                                    <a href="javascript: void(0);">Support</a>
-                                    <a href="javascript: void(0);">Contact Us</a>
-                                </div>
+                                <script>document.write(new Date().getFullYear())</script> © Sunlit
                             </div>
                         </div>
                     </div>
@@ -74,4 +60,5 @@
         @include('layout/right-sidebar')
 
         <div class="rightbar-overlay"></div>
+
 @endsection
