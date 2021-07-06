bfd6f87158225d4ffe962f7aa07c5cdb9bfb4093
Luk <lgastmans@gmail.com>
2021-07-06 15:40:06 +0530

    updated Seeder Run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        StateSeeder::class;
+        $this->call([
+            StateSeeder::class,
+        ]);
     }
 }
