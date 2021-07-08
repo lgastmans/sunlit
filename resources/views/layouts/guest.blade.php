dca4b168898fd84e435460209c940941e8725282
Quentin <quentin@octostudio.fr>
2021-07-08 09:55:59 +0530

    add guest page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('title', 'Sunlit | Inventory')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@include('layouts/header')
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<div class="content-container">
+    <div class="wrapper">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+            <div class="content">
+                
+                <div class="container-fluid" >
+                    
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <h4 class="page-title">@yield('page-title')</h4>
+                            </div>
+                        </div>
+                    </div>     
+                    <!-- end page title --> 
+                    
+                </div> <!-- container -->
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
-</html>
+                @yield('content')
+
+            </div>
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <script>document.write(new Date().getFullYear())</script> © Sunlit
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+       
+    </div>
+
+</div>
+
+@include('layouts/footer')
