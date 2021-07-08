d659717a12e8c14c2bd2ac243f3337ece70bf443
Quentin <quentin@octostudio.fr>
2021-07-08 10:11:05 +0530

    add register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,78 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section("content")
+<div class="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xxl-4 col-lg-5">
+                <div class="card">
+                    <!-- Logo-->
+                    <div class="card-header pt-4 pb-4 text-center bg-primary">
+                        <a href="index.html">
+                            <span><img src="/images/logo.png" alt="" height="18"></span>
+                        </a>
+                    </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+                    <div class="card-body p-4">
+                        @if ($errors->any())
+                            <div class="alert alert-danger" role="alert">
+                                <div >
+                                    <i class="dripicons-wrong me-2"></i><strong>{{ __('Whoops! Something went wrong.') }}</strong>
+                                </div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+                                <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Full Name</label>
+                                <input id="name" class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Enter your name" required autofocus >
+                            </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                            <div class="mb-3">
+                                <label for="emailaddress" class="form-label">Email address</label>
+                                <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Enter your email" required autofocus >
+                            </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" placeholder="Enter your password"/>
+                            </div>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
+                            <div class="mb-3">
+                                <label for="password_confirmation" class="form-label">Confim Password</label>
+                                <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="current-password" placeholder="Confirm your password"/>
+                            </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
+                            <div class="mb-3 text-center">
+                                <button class="btn btn-primary" type="submit"> Sign Up </button>
+                            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+                        </form>
+                    </div> <!-- end card-body -->
+                </div>
+                <!-- end card -->
 
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+                <div class="row mt-3">
+                    <div class="col-12 text-center">
+                        <p class="text-muted">Already have account? <a href="{{ route('login') }}" class="text-muted ms-1"><b>Log In</b></a></p>
+                    </div> <!-- end col-->
+                </div>
+                <!-- end row -->
+
+            </div> <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</div>
+
+@endsection
