e0968a3ec1f52a9216e0454ded8902f130c613e4
Quentin <quentin@octostudio.fr>
2021-07-08 10:44:16 +0530

    add verify-email page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,39 +1,56 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
+@section('content')
+    <div class="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xxl-4 col-lg-5">
+                    <div class="card">
+                        <!-- Logo -->
+                        <div class="card-header pt-4 pb-4 text-center bg-primary">
+                            <a href="index.html">
+                                <span><img src="images/logo.png" alt="" height="18"></span>
+                            </a>
+                        </div>
 
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        <div class="card-body p-4">
+                            
+                            <div class="text-center m-auto">
+                                <img src="assets/images/mail_sent.svg" alt="mail sent image" height="64" />
+                                <h4 class="text-dark-50 text-center mt-4 fw-bold">Please check your email</h4>
+                                <p class="text-muted mb-4">
+                                    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                                </p>
+                            </div>
+
+                            <div class="mt-4 flex items-center justify-between">
+                                <form method="POST" action="{{ route('verification.send') }}">
+                                    @csrf
+                    
+                                    <div>
+                                        <x-button>
+                                            {{ __('Resend Verification Email') }}
+                                        </x-button>
+                                    </div>
+                                </form>
+                    
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                    
+                                    <x-button>
+                                        {{ __('Log Out') }}
+                                    </x-button>
+                                </form>
+                            </div>
+
+                        </div> <!-- end card-body-->
+                    </div>
+                    <!-- end card-->
+                    
+                </div> <!-- end col -->
             </div>
-        @endif
-
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-
-                <div>
-                    <x-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-button>
-                </div>
-            </form>
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+            <!-- end row -->
         </div>
-    </x-auth-card>
-</x-guest-layout>
+        <!-- end container -->
+    </div>
+@endsection
