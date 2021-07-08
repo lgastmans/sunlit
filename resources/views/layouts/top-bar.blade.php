c7fe6a986a39c85ca7f7cbe658194f57d106c281
Quentin <quentin@octostudio.fr>
2021-07-08 10:19:46 +0530

    add logout

--- resources/views/layouts/top-bar.blade.php
+++ resources/views/layouts/top-bar.blade.php
@@ -46,10 +46,13 @@
 
 
                 <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <a href="javascript:void(0);" class="dropdown-item notify-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="mdi mdi-logout me-1"></i>
                     <span>Logout</span>
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
         </li>
 
