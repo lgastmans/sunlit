a9faae513c01fb844883da496144ec13e9633abe
Luk <lgastmans@gmail.com>
2021-07-08 15:56:23 +0530

    updates to suppliers table

---
+++ database/migrations/2021_07_08_091547_update_suppliers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->string('address2')->nullable()->after('address');
+
+            $table->foreignId('state_id')->nullable()->change();
+            $table->string('gstin')->nullable()->change();
+            $table->string('phone2')->nullable()->change();
+            $table->string('contact_person')->nullable()->change();
+            $table->string('email')->nullable()->change();
+
+            $table->dropColumn('district');
+
+            $table->renameColumn('company_title', 'company');
+            $table->renameColumn('zip', 'zip_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        $table->string('district');
+
+        $table->renameColumn('company', 'company_title');
+        $table->renameColumn('zip_code', 'zip');
+
+        $table->dropColumn('address2');
+    }
+}
