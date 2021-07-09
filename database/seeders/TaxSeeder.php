6b87086f45fce56b5c1ffe56a160e3318b6b5cc7
Luk <lgastmans@gmail.com>
2021-07-09 14:30:13 +0530

    updated tax seeder amounts without decimals

--- database/seeders/TaxSeeder.php
+++ database/seeders/TaxSeeder.php
@@ -22,23 +22,23 @@ class TaxSeeder extends Seeder
             ],
             [
                 'name' => 'GST 0.25%',
-                'amount' => 0.25,
+                'amount' => 25,
             ],
             [
                 'name' => 'GST 5%',
-                'amount' => 5,
+                'amount' => 500,
             ],
             [
                 'name' => 'GST 12%',
-                'amount' => 12,
+                'amount' => 1200,
             ],
             [
                 'name' => 'GST 18%',
-                'amount' => 18,
+                'amount' => 1800,
             ],
             [
                 'name' => 'GST 28%',
-                'amount' => 28,
+                'amount' => 2800,
             ],
         ]);        
     }
