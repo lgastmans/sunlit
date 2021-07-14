8b30cdba21f94ff589634025f9332a32bafdf6f6
Luk <lgastmans@gmail.com>
2021-07-14 17:43:38 +0530

    dealers model and migration

---
+++ database/migrations/2021_07_14_114337_create_dealers_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDealersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dealers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('state_id')->nullable();
+            $table->string('company');
+            $table->string('address');
+            $table->string('address2')->nullable();
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('gstin')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->string('phone');
+            $table->string('phone2')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+            $table->softDeletes();            
+        });
+
+        Schema::table('dealers', function (Blueprint $table) {
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dealers', function (Blueprint $table) {
+            $table->dropForeign('dealers_state_id_foreign');
+        });
+
+        Schema::dropIfExists('dealers');
+    }
+}
