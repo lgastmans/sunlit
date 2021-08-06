a8b9aa7afc676ec98289d14d9649eb684e76ed0f
Luk <lgastmans@gmail.com>
2021-08-06 12:07:20 +0530

    products drop unique index on field code

---
+++ database/migrations/2021_08_06_063034_products_drop_uniqueindex.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsDropUniqueindex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropUnique(['code']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('code')->unique();
+        });
+    }
+}
