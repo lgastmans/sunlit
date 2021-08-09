f990675e8aa188148bfe336f402f4301291e1914
Luk <lgastmans@gmail.com>
2021-08-09 17:58:47 +0530

    Inventory

---
+++ database/migrations/2021_08_09_114748_create_inventories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_order_item_id');
+            $table->foreignId('warehouse_id');
+            $table->foreignId('product_id');
+            $table->integer('current_stock');
+            $table->integer('landed_cost')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
