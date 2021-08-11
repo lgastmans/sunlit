8c5643795ab88e3cf3e75942cfa1dc44c79bc79d
Luk <lgastmans@gmail.com>
2021-08-11 16:38:39 +0530

    added % format to excel export

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -8,12 +8,18 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
 /*
     https://makitweb.com/export-data-in-excel-and-csv-format-with-laravel-excel/
     https://laraveldaily.com/laravel-excel-export-formatting-and-styling-cells/
+    https://docs.laravel-excel.com/2.1/reference-guide/formatting.html
 */
 
-class ProductsExport implements FromCollection, WithHeadings, ShouldAutoSize
+class ProductsExport implements FromCollection, WithHeadings, ShouldAutoSize, WithColumnFormatting, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -26,7 +32,7 @@ class ProductsExport implements FromCollection, WithHeadings, ShouldAutoSize
                 ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id')
                 ->join('taxes', 'taxes.id', '=', 'products.tax_id')
                 ->orderBy('products.code', 'ASC')
-                ->get(['categories.name as category_name', 'suppliers.company', 'taxes.name as tax_name', 'products.code', 'products.name', 'products.model', 'products.cable_length', 'products.kw_rating', 'products.part_number']
+                ->get(['categories.name as category_name', 'suppliers.company', 'taxes.amount as tax_name', 'products.code', 'products.name', 'products.model', 'products.cable_length', 'products.kw_rating', 'products.part_number']
             ));
 
         //getProducts()
@@ -37,4 +43,31 @@ class ProductsExport implements FromCollection, WithHeadings, ShouldAutoSize
            "category", "supplier", "tax", "code","name","model","cable_length", "kw_rating", "part_number"
         ];
     }
+
+    public function columnFormats(): array
+    {
+        // return [
+        //     'G' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+        // ];
+
+        return [
+            'C' => NumberFormat::FORMAT_PERCENTAGE_00,
+        ];
+    }
+
+    public function map($product): array
+    {
+        return [
+            $product->category_name,
+            $product->company,
+            ($product->tax_name/10000),
+            $product->code,
+            $product->name,
+            $product->model,
+            $product->cable_length,
+            $product->kw_rating,
+            $product->part_number
+        ];
+    }
+
 }
