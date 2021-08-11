882b0a89c74fee4ec1894ba91eb6e3c5ed49b2b5
Luk <lgastmans@gmail.com>
2021-08-11 10:02:06 +0530

    excel export #61 completed

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -3,16 +3,36 @@
 namespace App\Exports;
 
 use App\Models\Product;
+
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ProductsExport implements FromCollection
+/*
+    https://makitweb.com/export-data-in-excel-and-csv-format-with-laravel-excel/
+*/
+
+class ProductsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Product::all();
+        //return collect(Product::all());
+
+        return collect(Product::join('categories', 'categories.id', '=', 'products.category_id')
+                ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id')
+                ->join('taxes', 'taxes.id', '=', 'products.tax_id')
+                ->orderBy('products.code', 'ASC')
+                ->get(['categories.name as category_name', 'suppliers.company', 'taxes.name as tax_name', 'products.code', 'products.name', 'products.model', 'products.cable_length', 'products.kw_rating', 'products.part_number']
+            ));
+
+        //getProducts()
+    }
+
+    public function headings(): array {
+        return [
+           "category", "supplier", "tax", "code","name","model","cable_length", "kw_rating", "part_number"
+        ];
     }
 }
