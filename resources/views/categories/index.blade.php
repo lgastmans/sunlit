e5cf6e06dc93f42532ec2ad95f5acce051f4eed5
Quentin <qdecosne@gmail.com>
2021-08-31 10:02:06 +0530

    remove categories datatables checkboxes #84

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -27,12 +27,7 @@
                     <table class="table table-centered table-borderless table-hover w-100 dt-responsive nowrap" id="categories-datatable">
                         <thead class="table-light">
                             <tr>
-                                <th style="width: 20px;">
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="customCheck1">
-                                        <label class="form-check-label" for="customCheck1">&nbsp;</label>
-                                    </div>
-                                </th>
+
                                 <th>Name</th>
                                 <th style="width:10%">
                                     @if (Auth::user()->can('edit categories'))
@@ -83,20 +78,6 @@
         },
         "pageLength": 10,
         "columns": [
-            {
-                'data': 'id',
-                'orderable': false,
-                'render': function (data, type, row, meta) {
-                    if (type === 'display') {
-                        data = "<div class=\"form-check\"><input type=\"checkbox\" class=\"form-check-input dt-checkboxes\"><label class=\"form-check-label\">&nbsp;</label></div>";
-                    }
-                    return data;
-                },
-                'checkboxes': {
-                    'selectRow': true,
-                    'selectAllRender': '<div class=\"form-check\"><input type=\"checkbox\" class=\"form-check-input dt-checkboxes\"><label class=\"form-check-label\">&nbsp;</label></div>'
-                }
-            },
             { 
                 'data': 'name',
                 'orderable': true 
