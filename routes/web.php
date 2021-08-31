8f0a92bbf8e1a1f39ba8f2c2f5fb968d44468500
Quentin <qdecosne@gmail.com>
2021-08-31 09:23:09 +0530

    remove duplicate route

--- routes/web.php
+++ routes/web.php
@@ -135,7 +135,5 @@ Route::group(['middleware' => ['auth']], function () {
 
     // Test routes to delete 
     Route::get('/purchase-orders/test', [PurchaseOrderController::class, 'test_purchase_orders'])->name('test');
-    Route::get('/purchase-orders/list', [PurchaseOrderController::class, 'getListForDatatables'])->name('purchase-orders.datatables');
-
     
 });
