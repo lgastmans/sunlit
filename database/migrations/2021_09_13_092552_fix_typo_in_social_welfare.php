f07ec00ddb40ddc5e8d2352e7b2fac06e71cd95f
Quentin <qdecosne@gmail.com>
2021-09-13 14:57:04 +0530

    fix typo in column name

---
+++ database/migrations/2021_09_13_092552_fix_typo_in_social_welfare.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixTypoInSocialWelfare extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            $table->renameColumn('social_welfare_surchage', 'social_welfare_surcharge');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            $table->renameColumn('social_welfare_surcharge', 'social_welfare_surchage');
+        });
+    }
+}
