7e99133c43a6fc3fc5a915d2c3ff606f9ee33a2f
Luk <lgastmans@gmail.com>
2021-10-05 15:09:34 +0530

    Inventory  Movement price field updates

---
+++ database/migrations/2021_10_05_063408_inventories_update_prices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InventoriesUpdatePrices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('inventories', function (Blueprint $table) {
+            $table->renameColumn('average_cost', 'average_selling_price')->nullable();
+            $table->decimal('average_buying_price', $precision = 13, $scale = 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('inventories', function (Blueprint $table) {
+            $table->renameColumn('average_selling_price', 'average_cost');
+            $table->dropColumn('average_buying_price');
+        });
+    }
+}
