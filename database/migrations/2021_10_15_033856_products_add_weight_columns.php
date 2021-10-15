977a4db6e3715aec46d5808c50bcdfca71d10452
Luk <lgastmans@gmail.com>
2021-10-15 09:29:26 +0530

    Product import table migration #92

---
+++ database/migrations/2021_10_15_033856_products_add_weight_columns.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsAddWeightColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('cable_length');
+            $table->decimal('cable_length_input', $precision = 13, $scale = 2)->nullable();
+            $table->decimal('cable_length_output', $precision = 13, $scale = 2)->nullable();
+            $table->decimal('weight_actual', $precision = 13, $scale = 2)->nullable();
+            $table->decimal('weight_volume', $precision = 13, $scale = 2)->nullable();
+            $table->decimal('weight_calculated', $precision = 13, $scale = 2)->nullable();
+            $table->smallInteger('warranty')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('cable_length')->nullable();
+            $table->dropColumn('cable_length_input');
+            $table->dropColumn('cable_length_output');
+            $table->dropColumn('weight_actual');
+            $table->dropColumn('weight_volume');
+            $table->dropColumn('weight_calculated');
+            $table->dropColumn('warranty');
+        });
+    }
+}
