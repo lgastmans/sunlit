1b61b27e358def5f9258af60ef90fabcebf8ee00
Luk <lgastmans@gmail.com>
2021-11-15 12:14:50 +0530

    new status blocked, created migragion #262

---
+++ database/migrations/2021_11_15_063816_inventory_add_blocked_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InventoryAddBlockedColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('inventories', function (Blueprint $table) {
+            $table->integer('stock_blocked')->nullable()->after('stock_ordered');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('inventories', function (Blueprint $table) {
+            $table->dropColumn('stock_blocked');
+        });
+    }
+}
