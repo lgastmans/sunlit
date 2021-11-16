7e8e845f559201b0752bfe50192c7a3833245807
Quentin <qdecosne@gmail.com>
2021-11-16 10:14:14 +0530

    create indexes warehouses #159

---
+++ database/migrations/2021_11_16_043923_add_indexes_warehouses.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexesWarehouses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('warehouses', function (Blueprint $table) {
+            $table->index('name');
+            $table->index('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('warehouses', function (Blueprint $table) {
+            $table->dropIndex('warehouses_name_index');
+            $table->dropIndex('warehouses_city_index');
+        });
+    }
+}
