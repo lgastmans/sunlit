abe66300c734dc255091e5b067c32c1aa6508eee
Quentin <qdecosne@gmail.com>
2021-11-16 10:14:41 +0530

    create indexes purchase orders #159

---
+++ database/migrations/2021_11_16_044143_add_indexes_purchase_orders.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexesPurchaseOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            $table->index('order_number');
+            $table->index('ordered_at');
+            $table->index('due_at');
+            $table->index('amount_inr');
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            $table->dropIndex('purchase_orders_order_number_index');
+            $table->dropIndex('purchase_orders_ordered_at_index');
+            $table->dropIndex('purchase_orders_due_at_index');
+            $table->dropIndex('purchase_orders_amount_inr_index');
+            $table->dropIndex('purchase_orders_status_index');
+        });
+
+    }
+}
