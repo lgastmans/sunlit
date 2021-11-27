ec8b52af402e231563a938146d3ff7567c28ee46
Luk <lgastmans@gmail.com>
2021-11-27 10:40:27 +0530

    calculate avg price on selling_price_inr #287

---
+++ database/migrations/2021_11_27_050436_add_purchase_order_invoice_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPurchaseOrderInvoiceId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->unsignedBigInteger('purchase_order_invoice_id')->after('purchase_order_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+           $table->dropColumn('purchase_order_invoice_id'); 
+        });
+    }
+}
