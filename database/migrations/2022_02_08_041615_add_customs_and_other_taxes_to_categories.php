3d516fa218d627a8b361853aac4a02e1aaad54de
Quentin <qdecosne@gmail.com>
2022-02-08 09:51:18 +0530

    add migration to move customs duty and co to categories

---
+++ database/migrations/2022_02_08_041615_add_customs_and_other_taxes_to_categories.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCustomsAndOtherTaxesToCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->decimal('customs_duty', $precision = 13, $scale = 2)->nullable()->after('hsn_code');
+            $table->decimal('social_welfare_surcharge', $precision = 13, $scale = 2)->nullable()->after('customs_duty');
+            $table->decimal('igst', $precision = 13, $scale = 2)->nullable()->after('social_welfare_surcharge');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('customs_duty');
+            $table->dropColumn('social_welfare_surcharge');
+            $table->dropColumn('igst');
+        });
+    }
+}
