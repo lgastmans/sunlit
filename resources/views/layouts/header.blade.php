215657fdaa1fc1ecc483ed35b5fe94255a256883
Luk <lgastmans@gmail.com>
2022-03-15 15:46:29 +0530

    SO proforma css in header file #351

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,11 @@
     <!-- third party css -->
     <link href="{{ asset("/css/vendor/dataTables.bootstrap4.css") }}" rel="stylesheet" type="text/css" /> 
     <link href="{{ asset("/css/vendor/responsive.bootstrap4.css") }}" rel="stylesheet" type="text/css" /> 
-    <link href="{{ asset("/css/proforma.css") }}" rel="stylesheet" type="text/css" /> 
+
+    @if (Request::segment(3) == "proforma")
+        <link href="{{ asset("/css/proforma.css") }}" rel="stylesheet" type="text/css" /> 
+    @endif
+    
     <!-- third party css end -->
 
     <!-- App css -->
