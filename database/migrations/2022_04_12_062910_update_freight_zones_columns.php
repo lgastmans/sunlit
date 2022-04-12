69e12a83f5e6dc83b9e4728d4694c8219ce9e97f
Luk <lgastmans@gmail.com>
2022-04-12 12:10:57 +0530

    updated columns freight zones #354

---
+++ database/migrations/2022_04_12_062910_update_freight_zones_columns.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFreightZonesColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('freight_zones', function (Blueprint $table) {
+            $table->dropColumn('states');
+            $table->decimal('rate_per_kg', 6, 2)->after('zone');
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('freight_zones', function (Blueprint $table) {
+            $table->string('states');
+            $table->dropColumn('rate_per_kg')->default(8);
+        });        
+    }
+}
