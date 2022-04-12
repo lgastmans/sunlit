602178d184b01525caa3eeb510ce6c12123b964e
Luk <lgastmans@gmail.com>
2022-04-12 12:11:28 +0530

    added column freight_zone_id to dealers #355

---
+++ database/migrations/2022_04_12_063536_update_dealers_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateDealersColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('dealers', function (Blueprint $table) {
+            $table->integer('freight_zone_id')->after('state_id')->nullable();
+        });          
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('dealers', function (Blueprint $table) {
+            $table->dropColumn('freight_zone_id');
+        });          
+    }
+}
