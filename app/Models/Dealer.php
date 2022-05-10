7db933c84b36d421effebf9a1de5bff8f1440fed
Luk <lgastmans@gmail.com>
2022-05-10 13:37:44 +0530

    Dealer model #380

--- app/Models/Dealer.php
+++ app/Models/Dealer.php
@@ -11,7 +11,7 @@ class Dealer extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['state_id', 'company', 'address', 'address2', 'city', 'zip_code', 'gstin', 'contact_person', 'phone', 'phone2', 'email'];
+    protected $fillable = ['state_id', 'company', 'address', 'address2', 'city', 'zip_code', 'gstin', 'contact_person', 'phone', 'phone2', 'email', 'has_shipping_address', 'shipping_state_id', 'shipping_company', 'shipping_address', 'shipping_address2', 'shipping_city', 'shipping_zip_code', 'shipping_gstin', 'shipping_contact_person', 'shipping_phone', 'shipping_phone2', 'shipping_email'];
     protected $with = ['state'];
 
     /**
@@ -20,6 +20,15 @@ class Dealer extends Model
     public function state()
     {
         return $this->belongsTo(State::class);
+    }
+
+    /**
+     * Get the shipping state associated with the dealer.
+     */
+    public function shipping_state()
+    {
+        return $this->belongsTo(State::class);
+        asdf
     }
 
 }
