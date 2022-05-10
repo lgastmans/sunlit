5f05975440015f7c40ebf740a73d6f1e063ffc6e
Luk <lgastmans@gmail.com>
2022-05-10 12:33:19 +0530

    SO: PDF export removed css reference #385

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,9 +16,9 @@
     <link href="{{ asset("/css/vendor/dataTables.bootstrap4.css") }}" rel="stylesheet" type="text/css" /> 
     <link href="{{ asset("/css/vendor/responsive.bootstrap4.css") }}" rel="stylesheet" type="text/css" /> 
 
-    @if (Request::segment(3) == "proforma")
+{{--     @if (Request::segment(3) == "proforma")
         <link href="{{ asset("/css/proforma.css") }}" rel="stylesheet" type="text/css" /> 
-    @endif
+    @endif --}}
     
     <!-- third party css end -->
 
