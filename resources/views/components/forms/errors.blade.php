cde8b03462818f67369103fabafd2043889e33f7
Luk <lgastmans@gmail.com>
2022-06-29 12:50:50 +0530

    rollback to original

--- resources/views/components/forms/errors.blade.php
+++ resources/views/components/forms/errors.blade.php
@@ -1,11 +1,11 @@
 @props(['errors'])
 
-{{-- @if ($errors->any()) --}}
-@if (count($errors))
+@if ($errors->any())
+{{-- @if (count($errors)) --}}
     <div class="alert alert-danger" role="alert">
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            {{-- @foreach ($errors->all() as $error) --}}
-            @foreach ($errors as $error)
+            @foreach ($errors->all() as $error)
+            {{-- @foreach ($errors as $error) --}}
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
