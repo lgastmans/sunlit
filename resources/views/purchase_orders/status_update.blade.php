b71aadbf9f2f46ed79c89fb78faf0ae4a37020e6
Luk Gastmans <lgastmans@users.noreply.github.com>
2023-04-04 15:26:52 +0530

    PO: the date input fields are set to accept up to one month in the past #506

--- resources/views/purchase_orders/status_update.blade.php
+++ resources/views/purchase_orders/status_update.blade.php
@@ -14,7 +14,7 @@
                     data-date-container="#confirmed_at"
                     data-date-autoclose="true"
                     data-date-format="M d, yyyy"
-                    data-date-start-date="-1d"
+                    data-date-start-date="-1m"
                     data-date-end-date="+6m"
                     data-date-today-highlight="true"
                     required>
@@ -49,7 +49,7 @@
                         data-date-container="#customs_at"
                         data-date-autoclose="true"
                         data-date-format="M d, yyyy"
-                        data-date-start-date="-1d"
+                        data-date-start-date="-1m"
                         data-date-end-date="+6m"
                         data-date-today-highlight="true"
                         required>
@@ -88,7 +88,7 @@
                         data-date-container="#cleared_at"
                         data-date-autoclose="true"
                         data-date-format="M d, yyyy"
-                        data-date-start-date="-1d"
+                        data-date-start-date="-1m"
                         data-date-end-date="+6m"
                         data-date-today-highlight="true"
                         required>
@@ -140,9 +140,15 @@
                     <div class="col-xl-6" id="received_at">
                         <label class="form-label">Received date</label>
                         <input type="text" class="form-control" name="received_at"
-                            value="{{ $purchase_order->display_received_at }}" data-provide="datepicker" data-date-autoclose="true"
-                            data-date-container="#received_at" data-date-format="M d, yyyy" data-date-today-highlight="true" data-date-start-date="-1d"
-                            data-date-end-date="+6m" required>
+                            value="{{ $purchase_order->display_received_at }}" 
+                            data-provide="datepicker" 
+                            data-date-autoclose="true"
+                            data-date-container="#received_at" 
+                            data-date-format="M d, yyyy" 
+                            data-date-today-highlight="true" 
+                            data-date-start-date="-1m"
+                            data-date-end-date="+6m" 
+                            required>
                     </div>
                 </div>
             
