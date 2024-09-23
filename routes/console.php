b5a54645c0ebbbd789b5f03b52a4bd0c86f7031b
Shift <shift@laravelshift.com>
2024-09-23 11:25:31 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
+// Schedule::command('exchangerate:update')->daily()->at('05:30');
+Schedule::command('exchangerate:update')->hourly();
