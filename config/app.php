14658701303808bbffdd10b9bf3cafddd380daa0
Shift <shift@laravelshift.com>
2024-09-23 11:25:49 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
-
 
     'aliases' => Facade::defaultAliases()->merge([
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-
 Schedule::command('backup:clean')->daily()->at('01:00');
 Schedule::command('backup:run')->daily()->at('01:30');
 // Schedule::command('exchangerate:update')->daily()->at('05:30');
